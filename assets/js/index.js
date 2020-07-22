c8e3ca4537046823ebf69797fdf3add0d8a6589e
Tosin Jimoh <jayoluwatosin@gmail.com>
2020-07-22 09:50:02 +0100

    Activate Preloader

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,8 @@
+const loader = document.querySelector(".loader");
 
+// Preloader fades after 3000ms  
+window.addEventListener('load', function () {
+  setTimeout(() => {
+    loader.className += " hidden";
+  }, 3000)
+});
